bae6a6213dda4787c1fd7747211c2f0220af4c48
JonatanLF <jonatan@heimo.no>
2024-01-20 13:34:14 +0100

    first commit

---
+++ .arduino/output/sketch/testkode.ino.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\jonat\\OneDrive\\Dokumenter\\Nokken\\testkode\\testkode.ino"
+void setup()
+{
+    //testtest
+}
+
+void loop()
+{
+    //testest
+}
---
+++ testkode.ino
@@ -0,0 +1,9 @@
+void setup()
+{
+    //testtest
+}
+
+void loop()
+{
+    //testest
+}
